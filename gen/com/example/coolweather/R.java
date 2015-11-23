0122beb3daa29d8dd480c387e06ab03ad890849e
Wyh <Wyh@163.com>
2015-11-23 14:02:17 +0800

    加入显示天气信息的功能

--- gen/com/example/coolweather/R.java
+++ gen/com/example/coolweather/R.java
@@ -1998,7 +1998,7 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09003b;
         public static final int action_mode_bar_stub=0x7f09003a;
         public static final int action_mode_close_button=0x7f090023;
-        public static final int action_settings=0x7f09005c;
+        public static final int action_settings=0x7f090063;
         public static final int activity_chooser_view_content=0x7f090024;
         public static final int alertTitle=0x7f09002e;
         public static final int always=0x7f090019;
@@ -2007,8 +2007,10 @@ containing a value of this type.
         public static final int cancel_action=0x7f090050;
         public static final int checkbox=0x7f090036;
         public static final int chronometer=0x7f090056;
+        public static final int city_name=0x7f09005c;
         public static final int collapseActionView=0x7f09001a;
         public static final int contentPanel=0x7f09002f;
+        public static final int current_date=0x7f09005f;
         public static final int custom=0x7f090033;
         public static final int customPanel=0x7f090032;
         public static final int decor_content_parent=0x7f09003c;
@@ -2039,6 +2041,7 @@ containing a value of this type.
         public static final int parentPanel=0x7f09002b;
         public static final int progress_circular=0x7f090007;
         public static final int progress_horizontal=0x7f090008;
+        public static final int publish_text=0x7f09005d;
         public static final int radio=0x7f090038;
         public static final int screen=0x7f090015;
         public static final int scrollView=0x7f090030;
@@ -2064,6 +2067,8 @@ containing a value of this type.
         public static final int status_bar_latest_event_content=0x7f090051;
         public static final int submit_area=0x7f090049;
         public static final int tabMode=0x7f09000c;
+        public static final int temp1=0x7f090061;
+        public static final int temp2=0x7f090062;
         public static final int text=0x7f090059;
         public static final int text2=0x7f090057;
         public static final int textSpacerNoButtons=0x7f090031;
@@ -2074,6 +2079,8 @@ containing a value of this type.
         public static final int topPanel=0x7f09002c;
         public static final int up=0x7f090001;
         public static final int useLogo=0x7f090013;
+        public static final int weather_desp=0x7f090060;
+        public static final int weather_info_layout=0x7f09005e;
         public static final int withText=0x7f09001d;
         public static final int wrap_content=0x7f090009;
     }
@@ -2151,6 +2158,7 @@ containing a value of this type.
         public static final int select_dialog_multichoice_material=0x7f030022;
         public static final int select_dialog_singlechoice_material=0x7f030023;
         public static final int support_simple_spinner_dropdown_item=0x7f030024;
+        public static final int weather_layout=0x7f030025;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
--- src/com/example/coolweather/util/Utility.java
+++ src/com/example/coolweather/util/Utility.java
@@ -1,7 +1,18 @@
 package com.example.coolweather.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.R.attr;
 import android.R.bool;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import com.example.coolweather.db.CoolWeatherDB;
@@ -62,5 +73,36 @@ public class Utility {
 			}
 		}
 		return false;
+	}
+	
+	public static void handleWeatherResponse(Context context,String response){
+		try {
+			JSONObject jsonObject = new JSONObject(response);
+			JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+			String cityName = weatherInfo.getString("city");
+			String weatherCode = weatherInfo.getString("cityid");
+			String temp1 = weatherInfo.getString("temp1");
+			String temp2 = weatherInfo.getString("temp2");
+			String weatherDesp = weatherInfo.getString("weather");
+			String publishTime = weatherInfo.getString("ptime");
+			saveWeatherInfo(context,cityName,weatherCode,temp1,temp2,weatherDesp,publishTime);
+		} catch (JSONException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	
+	public static void saveWeatherInfo(Context context,String cityName,String weatherCode,String temp1,String temp2,String weatherDesp,String publishTime){
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyƒÍM‘¬d»’", Locale.CHINA);
+		SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+		editor.putBoolean("city_selected", true);
+		editor.putString("city_name", cityName);
+		editor.putString("weather_code", weatherCode);
+		editor.putString("temp1", temp1);
+		editor.putString("temp2", temp2);
+		editor.putString("weather_desp", weatherDesp);
+		editor.putString("publish_time", publishTime);
+		editor.putString("current_date", sdf.format(new Date()));
+		editor.commit();
 	}
 }
